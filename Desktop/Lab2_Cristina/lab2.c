9ba91784b9804d5aadd024e9acca567ec1f7e8f3
ToeaMariana <mariana.toea4@gmail.com>
2024-03-07 13:30:14 +0200

    Modificare

---
+++ Desktop/Lab2_Cristina/lab2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+///recursivitate
+
+int function(int x){
+    if(x!=0)
+	{
+	    x=x-2;
+        x=function(x);
+        return x;
+	}
+	return 0;
+}
+
+int main(){
+	int a=30, i=0;
+
+	while (a>0){
+		a=function(a);
+		i++;
+	}
+	printf("%d", i);
+	return 0;
+}
+
